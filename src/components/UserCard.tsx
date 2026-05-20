1d02abe1ad5a5416f0ef95ed2e0f8d43ad6ca6e3
Jane Togo <lookatma@mail.ru>
2026-05-20 12:07:54 +0000

    style(ui): optimize CardMedia asset image alignment

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import {
+  alpha,
+  Box,
   Button,
   Card,
   CardActions,
@@ -23,7 +25,30 @@ export default function UserCard({ user, setOpen, setSelectedUser }: UserCardPro
 
   return (
     <Card elevation={3} sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      <CardMedia component="img" height="160" image={avatar} alt={name} />
+      <Box
+        sx={{
+          height: 160,
+          width: '100%',
+          backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.06),
+          display: 'flex',
+          justifyContent: 'center',
+        }}>
+        <CardMedia
+          component="img"
+          image={avatar}
+          alt={name}
+          sx={{
+            height: '100%',
+            width: 'auto',
+            objectFit: 'contain',
+          }}
+          onError={(e) => {
+            (e.target as HTMLImageElement).src =
+              'data:image/svg+xml;utf8,<svg xmlns="http://w3.org" width="100" height="100" viewBox="0 0 24 24" fill="%23ccc"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>';
+          }}
+        />
+      </Box>
+
       <CardContent sx={{ flexGrow: 1 }}>
         <Typography variant="h6" component="div" noWrap gutterBottom>
           {name}
