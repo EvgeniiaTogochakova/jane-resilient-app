2a2bef06014e6cf7ae709d4ba19948f5a5d99833
Jane Togo <lookatma@mail.ru>
2026-05-22 17:37:10 +0000

    refactor: clean up unused styles and simplify theme configuration

--- src/theme.ts
+++ src/theme.ts
@@ -13,25 +13,6 @@ const theme = createTheme({
       active: '#4C2F27',
     },
   },
-  // components: {
-  //   // Находим нужный компонент в "матрешке"
-  //   MuiTextField: {
-  //     defaultProps: {
-  //       variant: 'outlined', // Теперь все инпуты по умолчанию будут outlined
-  //       margin: 'normal', // И с нормальными отступами
-  //     },
-  //   },
-  //   MuiOutlinedInput: {
-  //     styleOverrides: {
-  //       root: {
-  //         borderRadius: '12px', // Делаем все поля скругленными
-  //         '&:hover .MuiOutlinedInput-notchedOutline': {
-  //           borderColor: '#E19D29', // Оранжевая рамка при наведении для всех полей
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
 });
 
 export default theme;
